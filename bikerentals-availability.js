5a811985b66a83f9bb1b9f6a10035f78372c82aa
Irina Plaksina <irene.plaksina@gmail.com>
2019-08-16 21:17:52 -0400

    initial commit

---
+++ bikerentals-availability.js
@@ -0,0 +1,32 @@
+let availability = [
+    {
+        "id": 1,
+        "name": "Adult Male Bike",
+        "quantity" : 10
+    },
+    {
+        "id": 2,
+        "name": "Adult Female Bike",
+        "quantity" : 10
+    },
+    {
+        "id": 3,
+        "name": "Kids Unisex Bike",
+        "quantity" : 5
+    },
+    {
+        "id": 4,
+        "name": "Adult Unisex Helmet",
+        "quantity" : 20
+    },
+    {
+        "id": 5,
+        "name": "Kids Unisex Helmet",
+        "quantity" : 5
+    },
+    {
+        "id": 6,
+        "name": "Insurance",
+        "quantity" : 50
+    }
+]
---
+++ inventory.js
@@ -0,0 +1,48 @@
+let bikes = [];
+bikes[0] = {
+    "id": 1,
+    "name": "Adult Male Bike",
+    "price": 20.50,
+    "image": "http://via.placeholder.com/250x250?text=Adult%20Male%20Bike",
+    "product_type": "bike"
+};
+bikes[1] =
+    {
+        "id": 2,
+        "name": "Adult Female Bike",
+        "price": 20.50,
+        "image": "http://via.placeholder.com/250x250?text=Adult%20Female%20Bike",
+        "product_type": "bike"
+    };
+bikes[2] =
+    {
+        "id": 3,
+        "name": "Kids Unisex Bike",
+        "price": 12.75,
+        "image": "http://via.placeholder.com/250x250?text=Kids%20Unisex%20Bike",
+        "product_type": "bike"
+    };
+bikes[3] =
+    {
+        "id": 4,
+        "name": "Adult Unisex Helmet",
+        "price": 4.00,
+        "image": "http://via.placeholder.com/250x250?text=Adult%20Unisex%20Helmet",
+        "product_type": "accessory"
+    };
+bikes[4] =
+    {
+        "id": 5,
+        "name": "Kids Unisex Helmet",
+        "price": 3.50,
+        "image": "http://via.placeholder.com/250x250?text=Kids%20Unisex%20Helmet",
+        "product_type": "accessory"
+    };
+bikes[5] =
+    {
+        "id": 6,
+        "name": "Insurance",
+        "price": 9.99,
+        "image": "http://via.placeholder.com/250x250?text=Insurance",
+        "product_type": "addon"
+    };
